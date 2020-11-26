3b791195fb3f585f98193f175bd843807e20ec2c
zhaokuo <zhaokuo@126.com>
2020-11-26 09:54:22 +0800

    在Person类中增加了属性

---
+++ TestModel/src/com/putress/Person.java
@@ -0,0 +1,12 @@
+package com.putress;
+
+/**
+ * @Auther: zhao_
+ * @Date: 2020/11/26 - 11 - 26 - 9:52
+ * @Description: com.putress
+ * @version: 1.0
+ */
+public class Person {
+    private int id;
+    private String name;
+}
